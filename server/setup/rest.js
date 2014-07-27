7de24597fdf6e78a27e5bdcc14511542d841ae62
Marshall Thompson <marshall@creativeideal.net>
2014-07-27 15:38:02 -0600

    A better body parser.

--- server/setup/rest.js
+++ server/setup/rest.js
@@ -3,5 +3,6 @@ var bodyParser = require('body-parser');
 module.exports = function(app, feathers){
 
 	app.configure(feathers.rest())
-	  .use(bodyParser());
+	  .use(bodyParser.json())
+	  .use(bodyParser.urlencoded({extended: true}));
 };
